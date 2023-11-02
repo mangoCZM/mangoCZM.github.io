1891604b4945776f5782d75b37787bf10e35780f
zm.c <zhimeng.chen@amh-group.com>
2023-11-02 10:28:55 +0800

    1

---
+++ docs/.vuepress/config.js
@@ -0,0 +1,42 @@
+module.exports = {
+  title: "Hello Vuepress",
+  description: "Vuepress Demo",
+  theme: "reco",
+  themeConfig: {
+    nav: [
+      { text: "首页", link: "/" },
+      {
+        text: "我的博客",
+        items: [
+          { text: "Github", link: "https://github.com" },
+          {
+            text: "掘金",
+            link: "https://juejin.cn",
+          },
+        ],
+      },
+    ],
+    sidebar: [
+      {
+        title: "欢迎学习",
+        path: "/",
+        collapsable: false, // 不折叠
+        children: [{ title: "学前必读", path: "/" }],
+      },
+      {
+        title: "基础学习",
+        path: "/handbook/ConditionalTypes",
+        collapsable: false, // 不折叠
+        children: [
+          { title: "条件类型", path: "/handbook/ConditionalTypes" },
+          { title: "泛型", path: "/handbook/Generics" },
+        ],
+      },
+    ],
+  },
+  locales: {
+    "/": {
+      lang: "zh-CN",
+    },
+  },
+};
---
+++ docs/handbook/ConditionalTypes.md
@@ -0,0 +1,17 @@
+---
+title: 条件类型
+author: Hello World
+date: "2023-12-12"
+---
+
+ConditionalTypes
+
+# 1
+
+* 1
+* 2
+* 3
+
+# 2
+
+## 3
