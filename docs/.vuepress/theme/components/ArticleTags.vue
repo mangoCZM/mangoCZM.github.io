ee50e1694e5d358c04f457d91c29b8458e4e4bfd
zm.c <zhimeng.chen@amh-group.com>
2023-11-03 14:54:55 +0800

    commit vuepress

---
+++ docs/.vuepress/theme/components/ArticleTags.vue
@@ -0,0 +1,36 @@
+<template>
+  <div class="article-tags">
+    <h1>{{ $page.title }}</h1>
+    <NiceTag :label="showAuthor" />
+    <NiceTag :label="showTime" />
+  </div>
+</template>
+
+<script>
+import NiceTage from "./NiceTag.vue";
+export default {
+  name: "ArticleTags",
+  components: { NiceTage },
+
+  computed: {
+    showTime() {
+      return new Date(this.$page.lastUpdatedTimestamp)
+        .toString()
+        .split(" ")
+        .slice(0, 4)
+        .join(" ");
+    },
+
+    showAuthor() {
+      return this.$site.themeConfig.author;
+    },
+  },
+};
+</script>
+
+<style lang="stylus" scoped>
+.article-tags{
+  padding: 0 2rem;
+}
+</style>
+
--- docs/handbook/ConditionalTypes.md
+++ docs/handbook/ConditionalTypes.md
@@ -3,6 +3,28 @@ title: 条件类型
 date: "2023-12-12"
 ---
 
-<ArticleTags />
+| Tables        | Are           | Cool  |
+| ------------- |:-------------:| -----:|
+| col 3 is      | right-aligned | $1600 |
+| col 2 is      | centered      |   $12 |
+| zebra stripes | are neat      |    $1 |
 
 ## 1
+
+``` js
+export default {
+  name: 'MyComponent',
+  // ...
+}
+```
+
+``` js {4}
+export default {
+  data () {
+    return {
+      msg: 'Highlighted!'
+    }
+  }
+}
+```
+
